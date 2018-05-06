f1970875dddfde04ad611140596ccd7b9a04807f
meghna <singhmeghna79@gmail.com>
2018-05-06 15:24:14 +0530

    add headers

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ function oauth_1()
     var url = "https://github.com/login/oauth/authorize";
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url, true ); // false for synchronous request
-    xmlHttp.setRequestHeader("ca24fda06e84714a41f4",client_id);
+    xmlHttp.setRequestHeader("ca24fda06e84714a41f4","client_id");
+    xmlHttp.setRequestHeader("cache-control", "no-cache");
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
